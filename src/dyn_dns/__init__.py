fae6fbd0a3570d0af09be522f0dffe51b64821af
schleising <stephen@schleising.net>
2024-08-04 16:23:33 +0100

    Reformat with Black

--- src/dyn_dns/__init__.py
+++ src/dyn_dns/__init__.py
@@ -1,9 +1,9 @@
-import sys
 import logging
 
 from pydantic import BaseModel, ValidationError
 
 from database.database import BackendDatabase
+
 
 # Model to store the dyn dns details
 class DynDnsDetails(BaseModel):
@@ -14,14 +14,15 @@ class DynDnsDetails(BaseModel):
     cloudflare_dns_record_id: str
     cloudflare_api_token: str
 
+
 # Get a backend database instance
 database = BackendDatabase()
 
 # Set the database in use
-database.set_database('web_database')
+database.set_database("web_database")
 
 # Set the collection in use
-DNS_INFO_COLLECTION = database.get_collection('dns_info')
+DNS_INFO_COLLECTION = database.get_collection("dns_info")
 
 if DNS_INFO_COLLECTION is not None:
     try:
@@ -34,15 +35,17 @@ if DNS_INFO_COLLECTION is not None:
             dyn_dns_details = DynDnsDetails(**dns_info)
         else:
             # If the dns_info is None, log an error and exit
-            logging.error('Failed to get the dyn dns details from the database.')
+            logging.error("Failed to get the dyn dns details from the database.")
             dyn_dns_details = None
     except ValidationError as e:
         # If the validation fails, log the error and exit
-        logging.error(f'Failed to get the dyn dns details from the database. Error: {e}')
+        logging.error(
+            f"Failed to get the dyn dns details from the database. Error: {e}"
+        )
         dyn_dns_details = None
 else:
     # If the collection is None, log an error and exit
-    logging.error('Failed to get the collection from the database.')
+    logging.error("Failed to get the collection from the database.")
     dyn_dns_details = None
 
 if dyn_dns_details is None:
@@ -50,7 +53,7 @@ if dyn_dns_details is None:
     CLOUDFLARE_HEADERS = None
 else:
     # Set the cloudflare URL to update the DNS record
-    CLOUDFLARE_DNS_UPDATE_URL = f'{dyn_dns_details.cloudflare_api_base_url}/zones/{dyn_dns_details.cloudflare_zone_id}/dns_records/{dyn_dns_details.cloudflare_dns_record_id}'
+    CLOUDFLARE_DNS_UPDATE_URL = f"{dyn_dns_details.cloudflare_api_base_url}/zones/{dyn_dns_details.cloudflare_zone_id}/dns_records/{dyn_dns_details.cloudflare_dns_record_id}"
 
     # Set the headers to include the api key
     CLOUDFLARE_HEADERS = {
@@ -60,16 +63,18 @@ else:
 
 # Get the Notify Run Endpoint
 try:
-    with open('src/secrets/notify_run_endpoint.txt', 'r', encoding='utf-8') as notify_run_endpoint_file:
+    with open(
+        "src/secrets/notify_run_endpoint.txt", "r", encoding="utf-8"
+    ) as notify_run_endpoint_file:
         NOTIFY_RUN_ENDPOINT = notify_run_endpoint_file.read().strip()
 except FileNotFoundError:
     # If the file is not found, log an error and set the NOTIFY_RUN_ENDPOINT to None
-    logging.error('Failed to get the notify run endpoint. File not found.')
+    logging.error("Failed to get the notify run endpoint. File not found.")
     NOTIFY_RUN_ENDPOINT = None
 
 from .dyn_dns import dyn_dns_loop
 
 # Export the dyn_dns_loop function
 __all__ = [
-    'dyn_dns_loop',
+    "dyn_dns_loop",
 ]
--- src/dyn_dns/dyn_dns.py
+++ src/dyn_dns/dyn_dns.py
@@ -9,7 +9,14 @@ from notify_run import Notify
 
 from task_scheduler import TaskScheduler
 
-from . import CLOUDFLARE_HEADERS, CLOUDFLARE_DNS_UPDATE_URL, NOTIFY_RUN_ENDPOINT, dyn_dns_details, DNS_INFO_COLLECTION
+from . import (
+    CLOUDFLARE_HEADERS,
+    CLOUDFLARE_DNS_UPDATE_URL,
+    NOTIFY_RUN_ENDPOINT,
+    dyn_dns_details,
+    DNS_INFO_COLLECTION,
+)
+
 
 class DynDns:
     def __init__(self, scheduler: TaskScheduler) -> None:
@@ -17,7 +24,9 @@ class DynDns:
         self.scheduler = scheduler
 
         # Schedule the task to check the external IP every minute and update the DNS if it has changed
-        self.scheduler.schedule_task(datetime.now(timezone.utc), self.update_dns, timedelta(minutes=1))
+        self.scheduler.schedule_task(
+            datetime.now(timezone.utc), self.update_dns, timedelta(minutes=1)
+        )
 
         # If the notify run endpoint is set, initialise the notify run object
         if NOTIFY_RUN_ENDPOINT:
@@ -28,27 +37,29 @@ class DynDns:
     def get_external_ip(self) -> str:
         # Check whether the dyn dns details are set
         if dyn_dns_details is None:
-            logging.error('Dyn DNS details are not set.')
-            return ''
+            logging.error("Dyn DNS details are not set.")
+            return ""
 
         # Get the current external IP
         try:
             response = requests.get(dyn_dns_details.get_external_ip_url)
         except requests.exceptions.RequestException as e:
-            logging.error(f'Failed to get the external IP address. Exception: {e}')
+            logging.error(f"Failed to get the external IP address. Exception: {e}")
             return dyn_dns_details.current_external_ip
 
         # If the request was successful, return the new IP address otherwise return the current one
         if response.status_code == 200:
             logging.debug(f'External IP address currently {response.json()["ip"]}')
-            return response.json()['ip']
+            return response.json()["ip"]
         else:
-            logging.error(f'Failed to get the external IP address. Status code: {response.status_code}')
+            logging.error(
+                f"Failed to get the external IP address. Status code: {response.status_code}"
+            )
             return dyn_dns_details.current_external_ip
 
     def update_cloudflare_dns(self, new_external_ip: str) -> bool:
         # Get the time in the Europe/London timezone
-        london_tz = ZoneInfo('Europe/London')
+        london_tz = ZoneInfo("Europe/London")
         london_time = datetime.now(london_tz)
 
         # Create the payload to update the DNS record
@@ -61,26 +72,33 @@ class DynDns:
         }
 
         # Log the new external IP
-        logging.info(f'Updating DNS record to {new_external_ip}.')
+        logging.info(f"Updating DNS record to {new_external_ip}.")
 
         # Send the request to update the DNS record
         try:
-            response = requests.request("PATCH", CLOUDFLARE_DNS_UPDATE_URL, json=payload, headers=CLOUDFLARE_HEADERS)
+            response = requests.request(
+                "PATCH",
+                CLOUDFLARE_DNS_UPDATE_URL,
+                json=payload,
+                headers=CLOUDFLARE_HEADERS,
+            )
         except requests.exceptions.RequestException as e:
-            logging.error(f'Failed to update the DNS record. Exception: {e}')
+            logging.error(f"Failed to update the DNS record. Exception: {e}")
             return False
 
         if response.status_code == 200:
-            logging.info(f'DNS record updated successfully to {new_external_ip}.')
+            logging.info(f"DNS record updated successfully to {new_external_ip}.")
             return True
         else:
-            logging.error(f'Failed to update the DNS record. Status code: {response.status_code}')
+            logging.error(
+                f"Failed to update the DNS record. Status code: {response.status_code}"
+            )
             return False
 
     def update_dns(self) -> None:
         # Check whether the dyn dns details are set
         if dyn_dns_details is None:
-            logging.error('Dyn DNS details are not set.')
+            logging.error("Dyn DNS details are not set.")
             return
 
         # Get the new external IP
@@ -89,27 +107,38 @@ class DynDns:
         # If the current external IP is different to the one stored in the dyn dns details, update the dns
         if new_external_ip != dyn_dns_details.current_external_ip:
             # Log that the external IP has changed
-            logging.info(f'External IP address has changed from {dyn_dns_details.current_external_ip} to {new_external_ip}.')
+            logging.info(
+                f"External IP address has changed from {dyn_dns_details.current_external_ip} to {new_external_ip}."
+            )
 
             if self.update_cloudflare_dns(new_external_ip):
                 # If the notify run endpoint is set, send a notification
                 if self.notify:
-                    self.notify.send(f'DNS Update Succeeded\nIP Address Changed from {dyn_dns_details.current_external_ip} to {new_external_ip}.')
+                    self.notify.send(
+                        f"DNS Update Succeeded\nIP Address Changed from {dyn_dns_details.current_external_ip} to {new_external_ip}."
+                    )
 
                 # If the DNS record was updated successfully, update the current external IP
                 dyn_dns_details.current_external_ip = new_external_ip
 
                 # Update the database with the new external IP
                 if DNS_INFO_COLLECTION is not None:
-                    DNS_INFO_COLLECTION.update_one({}, {'$set': {'current_external_ip': new_external_ip}})
+                    DNS_INFO_COLLECTION.update_one(
+                        {}, {"$set": {"current_external_ip": new_external_ip}}
+                    )
             else:
                 # Send a notification if the notify run endpoint is set
                 if self.notify:
-                    self.notify.send(f'DNS Update Failed\nTried to update to {new_external_ip} but it failed. Current IP Address is {dyn_dns_details.current_external_ip}.')
+                    self.notify.send(
+                        f"DNS Update Failed\nTried to update to {new_external_ip} but it failed. Current IP Address is {dyn_dns_details.current_external_ip}."
+                    )
+
 
 def dyn_dns_loop(terminate_event: Event, log_level: int) -> None:
     # Initialise logging
-    logging.basicConfig(format='Dyn DNS: %(asctime)s - %(levelname)s - %(message)s', level=log_level)
+    logging.basicConfig(
+        format="Dyn DNS: %(asctime)s - %(levelname)s - %(message)s", level=log_level
+    )
 
     # Create a task scheduler
     scheduler = TaskScheduler()
